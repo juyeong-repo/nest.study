4175779099b2c1b848d401d2e4f5f781f332513a
juyeong-repo <juyeong.park.tech@gmail.com>
2025-01-25 21:08:29 +0900

    feat : change structure and add movie crud

--- chapter2/chapter2.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Chapter2Service } from './chapter2.service';
-import { Chapter2Controller } from './chapter2.controller';
-
-@Module({
-  controllers: [Chapter2Controller],
-  providers: [Chapter2Service],
-})
-export class Chapter2Module {}
--- chapter2/dto/update-chapter2.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChapter2Dto } from './create-chapter2.dto';
-
-export class UpdateChapter2Dto extends PartialType(CreateChapter2Dto) {}
